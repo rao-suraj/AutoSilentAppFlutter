6a6ece979918bd6d06c41002182563880f194823
rao-suraj <surajrao250@gmail.com>
2023-12-24 11:22:18 +0530

    separated code and improved ui

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -1,8 +1,8 @@
 import 'package:auto_silent_app/gen/assets.gen.dart';
 import 'package:auto_silent_app/gen/fonts.gen.dart';
+import 'package:auto_silent_app/presentation/screens/widgets/floating_action_button.dart';
 import 'package:auto_silent_app/presentation/utils/app_icons.dart';
 import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
-import 'package:floating_action_bubble/floating_action_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:theme_provider/theme_provider.dart';
 
@@ -42,121 +42,89 @@ class _MainScreenState extends State<MainScreen>
         }
       },
       child: Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndDocked,
-          floatingActionButton: Padding(
-            padding: EdgeInsets.only(bottom: mediaQuery.height * 0.12),
-            child: FloatingActionBubble(
-              items: [
-                Bubble(
-                  title: "Add Settings",
-                  iconColor:  colorScheme.onPrimary,
-                  bubbleColor: colorScheme.surface,
-                  icon: Icons.add,
-                  titleStyle: TextStyle(fontSize: 20, color:  colorScheme.onPrimary,fontFamily: FontFamily.rubik,fontWeight: FontWeight.w600),
-                  onPress: () {
-                  },
-                ),
-                Bubble(
-                  title: "Add Profile",
-                  iconColor: colorScheme.onPrimary,
-                  bubbleColor: colorScheme.surface,
-                  icon: Icons.add,
-                  titleStyle: TextStyle(fontSize: 20, color: colorScheme.onPrimary,fontFamily: FontFamily.rubik,fontWeight: FontWeight.w600),
-                  onPress: () {
-                  },
-                ),
-                Bubble(
-                  title: "Add Schedule",
-                  iconColor: colorScheme.onPrimary,
-                  bubbleColor: colorScheme.surface,
-                  icon: Icons.add,
-                  titleStyle: TextStyle(fontSize: 20, color: colorScheme.onPrimary,fontFamily: FontFamily.rubik,fontWeight: FontWeight.w600),
-                  onPress: () {},
-                )
-              ],
-              onPress: () => _animationController.isCompleted
-                  ? _animationController.reverse()
-                  : _animationController.forward(),
-              iconColor: colorScheme.onPrimary,
-              animation: _animation,
-              backGroundColor: colorScheme.primary,
-              iconData: Icons.add,
-            ),
-          ),
-          body: SafeArea(
-              child: Column(
-            children: [
-              Expanded(
-                flex: 8,
-                child: Container(
-                  color: colorScheme.surface,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(
-                            "Auto Silent",
-                            style: TextStyle(
-                                fontFamily: FontFamily.rubik,
-                                fontSize: 25,
-                                fontWeight: FontWeight.w800,
-                                color: colorScheme.primary),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.endFloat,
+        floatingActionButton: MyFloatingActionButton(_animationController, _animation),
+            bottomNavigationBar:  SizedBox(
+              height: mediaQuery.height * 0.1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: colorScheme.surface,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: Container(
+                          color: colorScheme.surface,
+                          child: BottomBarDefault(
+                            items: navItems,
+                            backgroundColor: Colors.transparent,
+                            color: colorScheme.onSecondary,
+                            colorSelected: colorScheme.primary,
+                            indexSelected: visit,
+                            iconSize: 24,
+                            onTap: (int index) => setState(() {
+                              visit = index;
+                            }),
+                            titleStyle: const TextStyle(fontSize: 11),
                           ),
                         ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Assets.images.menuBar.svg(
-                              width: 40,
-                              height: 40,
-                              colorFilter: ColorFilter.mode(
-                                  colorScheme.onSecondary, BlendMode.srcIn),
-                            ))
-                      ]),
-                ),
-              ),
-              Expanded(
-                  flex: 82,
-                  child: Container(
-                    color: Colors.transparent,
-                    child: Center(
-                        child: TextButton(
-                      onPressed: () {
-                        ThemeProvider.controllerOf(context).nextTheme();
-                      },
-                      child: Text("Change Theme"),
-                    )),
-                  )),
+                      ),
+                    ],
+                  ),
+            ),
+        body: SafeArea(
+          child: Column(
+            children: [
               Expanded(
                 flex: 10,
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      width: mediaQuery.width * 0.3,
-                      color: colorScheme.surface,
-                    ),
-                    Container(
-                      color: colorScheme.surface,
-                      width: mediaQuery.width * 0.7,
-                      child: BottomBarDefault(
-                        items: navItems,
-                        backgroundColor: Colors.transparent,
-                        color: colorScheme.onSecondary,
-                        colorSelected: colorScheme.primary,
-                        indexSelected: visit,
-                        iconSize: 24,
-                        onTap: (int index) => setState(() {
-                          visit = index;
-                        }),
-                        titleStyle: const TextStyle(fontSize: 11),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          "Auto Silent",
+                          style: TextStyle(
+                              fontFamily: FontFamily.rubik,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w800,
+                              color: colorScheme.primary),
+                        ),
                       ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Assets.images.menuBar.svg(
+                          width: 40,
+                          height: 40,
+                          colorFilter: ColorFilter.mode(
+                              colorScheme.onSecondary, BlendMode.srcIn),
+                        ),
+                      )
+                    ]),
+              ),
+              Expanded(
+                flex: 80,
+                child: Container(
+                  color: Colors.transparent,
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () {
+                        ThemeProvider.controllerOf(context).nextTheme();
+                      },
+                      child: const Text("Change Theme"),
                     ),
-                  ],
+                  ),
                 ),
-              )
+              ),
             ],
-          ))),
+          ),
+        ),
+      ),
     );
   }
 }
