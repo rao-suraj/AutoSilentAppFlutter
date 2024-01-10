a3b0b54f2f8ec45e2f41b16c6017186eb21a8cc7
rao-suraj <surajrao250@gmail.com>
2024-01-10 11:54:51 +0530

    improved file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:auto_silent_app/di/get_it.dart';
-import 'package:auto_silent_app/presentation/cubits/calendar_cubit/calendar_cubit.dart';
-import 'package:auto_silent_app/presentation/cubits/session_cubit/session_cubit.dart';
 import 'package:auto_silent_app/presentation/screens/main_screen.dart';
 import 'package:auto_silent_app/presentation/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:theme_provider/theme_provider.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
